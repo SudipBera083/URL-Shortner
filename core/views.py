f86c605118dd72304862daae97efdeb84b1d1bca
sudip <sudipbera083@gmail.com>
2025-11-03 18:43:48 +0530

    template has been created along with home()

--- core/views.py
+++ core/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+import pyshorteners
 
-# Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    short_url = None
+    original_url = None
+
+    if request.method == 'POST':
+        original_url = request.POST.get('original_url')
+        if original_url:
+            try:
+                s = pyshorteners.Shortener()
+                short_url = s.dagd.short(original_url)
+            except Exception as e:
+                short_url = f"Error: {str(e)}"
+
+    return render(request, 'home.html', {'short_url': short_url, 'original_url': original_url})
